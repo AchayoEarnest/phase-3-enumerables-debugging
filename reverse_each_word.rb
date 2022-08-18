dde57c6c4fc60c915579c889d263b2d432d05a47
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-08-18 03:57:06 +0300

    debugging enumerables

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,51 @@
 # Write your code here
+require 'pry'
+
+# def reverse_each_word(sentence)
+#   words = sentence.split
+#   reversed_words = []
+
+#   words.each do |word|
+#     reversed_words << word.reverse
+#   end
+
+#   reversed_words.join(" ")
+# end
+
+# binding.pry
+# 0
+
+# reverse_each_word("Hello there, and how are you?")
+
+
+# # Refactored 1
+# def reverse_each_word(sentence)
+#   words = sentence.split
+#   reversed_words = words.map do |word|
+#     word.reverse
+#   end
+
+#   reversed_words.join(" ")
+# end
+
+# reverse_each_word("Hello there, and how are you?")
+
+
+# # Refactored 2
+# def reverse_each_word(sentence)
+#   words = sentence.split
+
+#   reversed_words = words.map (&:reverse)
+
+#   reversed_words.join(" ")
+# end
+
+# reverse_each_word("Hello there, and how are you?")
+
+
+# Refactored 3
+def reverse_each_word(sentence)
+  sentence.split.map (&:reverse).join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?")
